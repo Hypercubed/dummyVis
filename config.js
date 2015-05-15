285a96f90db003d3d5eb625828eca1d1d9aecec0
curran <curran.kelleher@gmail.com>
2015-05-15 12:30:31 -0700

    Created first pass of plugin

---
+++ config.js
@@ -0,0 +1,18 @@
+System.config({
+  "baseURL": "/",
+  "transpiler": "traceur",
+  "paths": {
+    "*": "*.js",
+    "github:*": "jspm_packages/github/*.js"
+  }
+});
+
+System.config({
+  "map": {
+    "d3": "github:mbostock/d3@3.5.5",
+    "model": "github:curran/model@0.2.2",
+    "traceur": "github:jmcriffey/bower-traceur@0.0.88",
+    "traceur-runtime": "github:jmcriffey/bower-traceur-runtime@0.0.88"
+  }
+});
+
